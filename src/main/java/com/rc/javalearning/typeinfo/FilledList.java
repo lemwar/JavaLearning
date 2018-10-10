9f841cc6757a9bf85cd258e824f23742ce73eb99
lemwwwar@gmail.com <Versetty18>
2018-10-10 23:51:48 +0500

    Chapter 14 : Generic types

---
+++ src/main/java/com/rc/javalearning/typeinfo/FilledList.java
@@ -0,0 +1,38 @@
+package com.rc.javalearning.typeinfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class CountedInteger {
+    private static long counter;
+    private final long id = counter++;
+    public String toString() {
+        return Long.toString(id);
+    }
+}
+
+public class FilledList<T> {
+    private Class<T> type;
+
+    private FilledList(Class<T> type) {
+        this.type = type;
+    }
+
+    private List<T> create(int nElements) {
+        List<T> result = new ArrayList<>();
+        for(int i = 0; i < nElements; i++) {
+            try {
+                result.add(type.newInstance());
+            } catch (InstantiationException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        FilledList<CountedInteger> f1 = new FilledList<>(CountedInteger.class);
+        f1.create(15);
+    }
+
+}
